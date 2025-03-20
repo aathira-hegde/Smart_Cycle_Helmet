471a5887d5a50021a2f74c022e60a9675cdf2b96
Aathira Vijaykumar Hegde <aathira@aathiras-air.home>
2025-03-20 22:22:07 +0000

    Updated FIFO reading for 27 data words in continuous mode

--- SensorIntegration/lsm6dsox_v1_0.py
+++ SensorIntegration/lsm6dsox_v1_0.py
@@ -5,6 +5,7 @@ from micropython import const
 a = []
 read_timer = Timer(0)
 x = 0
+w = 0
 # Define register addresses
 
 LSM6DSOX_I2C_ADDR 		= 	const(0x6A)
@@ -17,6 +18,7 @@ LSM6DSOX_INT2_CTRL 		= 	const(0x0E)
 LSM6DSOX_WHO_AM_I 		= 	const(0x0F)
 LSM6DSOX_CTRL1_XL 		= 	const(0x10)
 LSM6DSOX_CTRL2_G 		= 	const(0x11)
+LSM6DSOX_CTRL3_C		=	const(0x12)
 LSM6DSOX_WAKE_UP_SRC 	= 	const(0x1B)
 LSM6DSOX_TAP_SRC		=	const(0x1C)
 LSM6DSOX_FIFO_STATUS1	=	const(0x3A)
@@ -33,6 +35,7 @@ LSM6DSOX_FIFO_DO_YL		=  	const(0x7B)
 LSM6DSOX_FIFO_DO_YH		=	const(0x7C)
 LSM6DSOX_FIFO_DO_ZL		=  	const(0x7D)
 LSM6DSOX_FIFO_DO_ZH		=	const(0x7E)
+LSM6DSOX_CNTR_BDR_REG1	=	const(0x0B)
 
 # FIFO buffer threshold
 buf_wtm 				=	const(0x1A)
@@ -47,8 +50,7 @@ wu_lsb			=	const(0x1)
 wu_dur			= 	const(0x04)
 ff_th			=	const(0x3)
 ff_dur			=	const(0x5)
-int1_cfg		=	const(0b00101000)
-int2_cfg		= 	const(0b10010000)
+int1_cfg		=	const(0b00001000)
 
 
 
@@ -56,8 +58,8 @@ int2_cfg		= 	const(0b10010000)
 class Adafruit_LSM6DSOX:
     def __init__(self, pin_scl, pin_sda, freq):
         self.device = I2C(1, scl = pin_scl, sda = pin_sda, freq = freq)
-#         self.int1 = Pin(7)
-#         self.int2 = Pin(8)
+        self.int1 = Pin(32, Pin.IN)
+        self.int2 = Pin(33, Pin.IN)
     
     def _scan_(self):
         return self.device.scan()
@@ -76,9 +78,9 @@ class Adafruit_LSM6DSOX:
 
     def _load_settings_(self):
         
-# 		Register watermark threshold for the depth of FIFO
+# 		Register watermark threshold for the depth of FIFO, stop on WTM
         self._write_8(LSM6DSOX_FIFO_CTRL1, (buf_wtm & 0xFF))
-        self._write_8(LSM6DSOX_FIFO_CTRL2, (buf_wtm >> 8 & 0x01))
+        self._write_8(LSM6DSOX_FIFO_CTRL2, (0x80))
         
 # 		Register the batch data rate for the gyro and accelerometer
         self._write_8(LSM6DSOX_FIFO_CTRL3, (bdr_ac << 4 | bdr_gy))
@@ -94,10 +96,9 @@ class Adafruit_LSM6DSOX:
         self._write_8(LSM6DSOX_WAKE_UP_DUR, (wu_lsb << 4 & 0xff))
         self._write_8(LSM6DSOX_WAKE_UP_THS, (ff_dur & 0x80 | wu_dur))
         self._write_8(LSM6DSOX_FREE_FALL, (ff_dur << 3 | ff_th & 0x7))
-
-# 		Register interrupt control register - fifo related interrupts to INT1, free fall/inactivity to INT2
-        self._write_8(LSM6DSOX_INT1_CTRL, (int1_cfg))
-        self._write_8(LSM6DSOX_MD2_CFG, (int2_cfg))
+        
+# 		Dataready_pulsed (bit 7) set to latched mode will work as FIFO trigger
+        self._write_8(LSM6DSOX_CNTR_BDR_REG1, 0x00)
         
 
                                                     
@@ -105,19 +106,17 @@ class Adafruit_LSM6DSOX:
         while self._read_8(LSM6DSOX_WHO_AM_I) != 0x6c:
             return False
         self._load_settings_()
-#         self.interrupt_en()
         return True
         
-#     def interrupt_en(self):
-#         self.int1.irq(trigger=Pin.IRQ_FALLING, handler=self.read_data)
-#         int2.irq(trigger=Pin. IRQ_RISING, handler=read_data)
+
         
         
-def read_data(t):
+def read_data():
     global x
     print (f'Reading data at timer interrupt')
     i = 0
     y = p._read_16(LSM6DSOX_FIFO_STATUS1)
+    print(p._read_8(0x1A))
     x = int(y & 0x03FF)
     print(x)
     while (i <= x):
@@ -128,15 +127,22 @@ def read_data(t):
         d = p._read_16(LSM6DSOX_FIFO_DO_ZL)
         print((f), int(hex(b)), int(hex(c)), int(hex(d)))
         i += 1
-    
-        
-        
 
+        
 p = Adafruit_LSM6DSOX(Pin(20), Pin(22), freq = 100000)
-print(p.begin())
-read_timer.init(mode = Timer.PERIODIC, period = 10000, callback = read_data)
+p.begin()
 
+while True :
 
+    m = p._read_8(LSM6DSOX_FIFO_STATUS2)
+    w = m >> 7
+    if w == 1 :
+        read_data()
+        print('Data read over')
+        print(' ')
+    
+    
+    
 
     
     
@@ -151,4 +157,5 @@ read_timer.init(mode = Timer.PERIODIC, period = 10000, callback = read_data)
     
 
                       
+
 
